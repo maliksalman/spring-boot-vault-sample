b415f18b451d038b6994fd4df4f342462d212dea
Salman Malik <salman.the.malik@gmail.com>
2025-05-22 15:09:21 -0400

    Now using spring-cloud to interact with vault

--- add-secrets.sh
+++ add-secrets.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-echo
-echo "Adding secret keys ..."
+function createKeys {
 
-SECRET_NAME='spring-boot-vault-sample'
-curl -X POST \
-  -H "X-Vault-Token: my-root" \
-  -H "Content-Type: application/json" \
-  -d '{ "data": { "key1":"value1", "key2":"value2" }}' \
-  http://localhost:8200/v1/secret/data/${SECRET_NAME}
+  SECRET_NAME=$1
+  VALUE_PREFIX=$2
 
-echo
-echo "Secret keys in vault ..."
-curl -X GET \
-  -H "X-Vault-Token: my-root" \
-  http://localhost:8200/v1/secret/data/${SECRET_NAME} -s | jq .data.data
+  echo
+  echo "Adding secrets to ${SECRET_NAME} ..."
+  curl -s -X POST \
+    -H "X-Vault-Token: my-root" \
+    -H "Content-Type: application/json" \
+    -d "{ \"data\": { \"my.key1\":\"${VALUE_PREFIX}-value-1\", \"my.key2\":\"${VALUE_PREFIX}-value-2\" }}" \
+    http://localhost:8200/v1/secret/data/${SECRET_NAME} > /dev/null
+  curl -X GET \
+    -H "X-Vault-Token: my-root" \
+    http://localhost:8200/v1/secret/data/${SECRET_NAME} -s | jq .data.data
+}
+
+createKeys spring-boot-vault-sample default
+createKeys spring-boot-vault-sample/dev dev
+createKeys spring-boot-vault-sample/prod prod
--- src/main/java/com/smalik/sample/VaultDependantConfig.java
+++ src/main/java/com/smalik/sample/VaultDependantConfig.java
@@ -3,18 +3,13 @@ package com.smalik.sample;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.vault.annotation.VaultPropertySource;
-import org.springframework.vault.config.EnvironmentVaultConfiguration;
 
 @Configuration
-@Import(EnvironmentVaultConfiguration.class)
-@VaultPropertySource("secret/spring-boot-vault-sample")
 public class VaultDependantConfig {
 
     @Bean
-    public VaultedSecrets beanWithSecrets(@Value("${key1}") String secret1,
-                                          @Value("${key2}") String secret2) {
+    public VaultedSecrets beanWithSecrets(@Value("${my.key1}") String secret1,
+                                          @Value("${my.key2}") String secret2) {
         return new VaultedSecrets(secret1, secret2);
     }
 }
